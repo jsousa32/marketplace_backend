af1e10973336d1c41b450e470545ea27918a88ec
João Lucas <joao-rulez@hotmail.com>
2023-11-08 19:23:30 -0300

    initial commit

---
+++ src/main/java/com/example/marketpalce/MarketpalceApplication.java
@@ -0,0 +1,13 @@
+package com.example.marketpalce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketpalceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketpalceApplication.class, args);
+	}
+
+}
