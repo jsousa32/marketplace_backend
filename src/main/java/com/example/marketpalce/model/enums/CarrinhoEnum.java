a902c61b2adfaadd4718a189b48a23dff2da1679
João Lucas <joao-rulez@hotmail.com>
2023-11-09 20:57:29 -0300

    [FEAT]: adição de um status no carrinho de compras

---
+++ src/main/java/com/example/marketpalce/model/enums/CarrinhoEnum.java
@@ -0,0 +1,14 @@
+package com.example.marketpalce.model.enums;
+
+/**
+ * The Enum CarrinhoEnum
+ *
+ * @author João Lucas Silva de Sousa.
+ * @since 09/11/2023.
+ */
+public enum CarrinhoEnum {
+
+    APROVADO,
+    CANCELADO,
+    PENDENTE
+}
