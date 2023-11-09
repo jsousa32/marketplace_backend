ae5270bfc417fb8c6667d00a82f2903bbd5c2f1b
João Lucas <joao-rulez@hotmail.com>
2023-11-08 22:37:50 -0300

    [FEAT]: criação da regra de negocio para usuário

---
+++ src/main/java/com/example/marketpalce/business/UsuarioBusiness.java
@@ -0,0 +1,108 @@
+package com.example.marketpalce.business;
+
+import com.example.marketpalce.handler.GlobalException;
+import com.example.marketpalce.model.dtos.Usuario;
+import com.example.marketpalce.model.schemas.UsuarioSchema;
+import com.example.marketpalce.repository.UsuarioRepository;
+import com.example.marketpalce.service.KeycloakInstanceService;
+import jakarta.ws.rs.core.Response;
+import org.keycloak.admin.client.resource.UserResource;
+import org.keycloak.admin.client.resource.UsersResource;
+import org.keycloak.representations.idm.UserRepresentation;
+import org.springframework.beans.BeanUtils;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+
+import java.util.Objects;
+import java.util.Random;
+
+import static com.example.marketpalce.utils.MapperUtils.mapper;
+import static com.example.marketpalce.utils.MapperUtils.userRepresentantion;
+
+/**
+ * The Class UsuarioBusiness
+ *
+ * @author João Lucas Silva de Sousa.
+ * @since 08/11/2023.
+ */
+@Component
+public class UsuarioBusiness {
+
+    private final UsuarioRepository usuarioRepository;
+    private final KeycloakInstanceService keycloakInstanceService;
+
+    public UsuarioBusiness(UsuarioRepository usuarioRepository,
+                           KeycloakInstanceService keycloakInstanceService) {
+        this.usuarioRepository = usuarioRepository;
+        this.keycloakInstanceService = keycloakInstanceService;
+    }
+
+    public void save(Usuario usuario) {
+        Boolean isExists = usuarioRepository.existsByEmail(usuario.getEmail());
+
+        if (isExists) {
+            throw new GlobalException("O email já está cadastrado", HttpStatus.CONFLICT);
+        }
+
+        UserRepresentation userRepresentation = userRepresentantion(usuario);
+
+        UsersResource userResource = keycloakInstanceService.getUserResource();
+
+        Response response = userResource.create(userRepresentation);
+
+        if (response.getStatus() != 201) {
+            throw new GlobalException("Problemas ao criar o usuário no keycloak", HttpStatus.CONFLICT);
+        }
+
+        String location = response.getMetadata().get("Location").toString().replace("]", "");
+
+        int index = location.lastIndexOf("users/");
+
+        String usuarioIdKeycloak = location.substring(index + "users/".length());
+
+        Integer tokenEmail = new Random().nextInt(999999);
+
+        usuario.setId(usuarioIdKeycloak);
+        usuario.setTokenEmail(tokenEmail.toString());
+
+        usuarioRepository.save(mapper(usuario, UsuarioSchema.class));
+
+    }
+
+    public Page<Usuario> findAll(Pageable pageable) {
+        return usuarioRepository.findAll(pageable)
+                .map(u -> mapper(u, Usuario.class));
+    }
+
+    public void update(String usuarioId, Usuario usuario) {
+        Usuario usuarioDatabase = mapper(usuarioRepository.getReferenceById(usuarioId), Usuario.class);
+
+        if (Objects.isNull(usuarioDatabase)) {
+            throw new GlobalException("Não foi possível localizar o usuário com o Id " + usuarioId, HttpStatus.BAD_REQUEST);
+        }
+
+        BeanUtils.copyProperties(usuario, usuarioDatabase);
+
+        UserRepresentation userRepresentation = userRepresentantion(usuarioDatabase);
+
+        UserResource userResource = keycloakInstanceService.getUserResource().get(usuarioDatabase.getId());
+
+        userResource.update(userRepresentation);
+
+        usuarioRepository.save(mapper(usuarioDatabase, UsuarioSchema.class));
+    }
+
+    public void delete(String usuarioId) {
+
+        Usuario usuario = mapper(usuarioRepository.getReferenceById(usuarioId), Usuario.class);
+
+        if (Objects.isNull(usuario)) {
+            throw new GlobalException("Não foi possível localizar o usuário com o Id " + usuarioId, HttpStatus.BAD_REQUEST);
+        }
+
+        usuarioRepository.delete(mapper(usuario, UsuarioSchema.class));
+
+    }
+}
