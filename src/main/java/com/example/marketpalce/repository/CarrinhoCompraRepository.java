a080aeb43ce20117410c10d0f7f40d81ba66906d
João Lucas <joao-rulez@hotmail.com>
2023-11-09 20:57:43 -0300

    [FIX]: implementação de novas querys

--- src/main/java/com/example/marketpalce/repository/CarrinhoCompraRepository.java
+++ src/main/java/com/example/marketpalce/repository/CarrinhoCompraRepository.java
@@ -1,8 +1,11 @@
 package com.example.marketpalce.repository;
 
 import com.example.marketpalce.model.schemas.CarrinhoCompraSchema;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 /**
@@ -12,4 +15,8 @@ import java.util.UUID;
  * @since 08/11/2023.
  */
 public interface CarrinhoCompraRepository extends JpaRepository<CarrinhoCompraSchema, UUID> {
+
+    Page<CarrinhoCompraSchema> findAllByUsuarioIdAndDisabled(String usuarioId, Boolean disabled, Pageable pageable);
+
+    Optional<CarrinhoCompraSchema> findByIdAndDisabled(UUID carrinhoId, Boolean disabled);
 }
