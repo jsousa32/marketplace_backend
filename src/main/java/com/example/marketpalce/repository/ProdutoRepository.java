0c3e97323d9d981cdcfdb96a973d46cfab5156f1
João Lucas <joao-rulez@hotmail.com>
2023-11-09 20:55:33 -0300

    [FIX]: correções no produto repository

--- src/main/java/com/example/marketpalce/repository/ProdutoRepository.java
+++ src/main/java/com/example/marketpalce/repository/ProdutoRepository.java
@@ -1,8 +1,11 @@
 package com.example.marketpalce.repository;
 
 import com.example.marketpalce.model.schemas.ProdutoSchema;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 /**
@@ -12,4 +15,10 @@ import java.util.UUID;
  * @since 08/11/2023.
  */
 public interface ProdutoRepository extends JpaRepository<ProdutoSchema, UUID> {
+
+    Boolean existsByCodigoBarrasAndDisabled(Double codigo, Boolean disabeld);
+
+    Page<ProdutoSchema> findAllByDisabled(Boolean disabeld, Pageable pageable);
+
+    Optional<ProdutoSchema> findByIdAndDisabled(UUID produtoId, Boolean disabled);
 }
